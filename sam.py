9bdeccdfba3538d8e57ccdac0af0bfcfe900d625
S A M E E R <87935151+sameerpanhwarit@users.noreply.github.com>
2022-03-06 04:46:51 -0800

    Create sam.py

---
+++ sam.py
@@ -0,0 +1,15 @@
+from dataclasses import replace
+import platform
+import os
+xox = platform.platform()[::-1].replace('.','').replace(' ','').replace('-','').upper()
+svr = requests.get('https://raw.githubusercontent.com/sameerpanhwarit/test/new/main/server.txt').text
+
+print('This is Your token: ',xox)
+
+print('-'*50)
+
+if xox in svr:
+    print("Your Subscription On.")
+
+else:
+    print("Please Buy This Tool")
