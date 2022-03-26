17188cc7327727e62420b4e9e62ed5013c78a098
S A M E E R <87935151+sameerpanhwarit@users.noreply.github.com>
2022-03-26 12:43:35 -0700

    Update Code.py

--- Code.py
+++ Code.py
@@ -108,7 +108,7 @@ def login_select():
             print ''
             print '\x1b[0;91mSuccessful > \x1b[0;94m' + nm
             time.sleep(5)
-           b_menu_select()
+            b_menu_select()
         except (KeyError, IOError):
             os.system('echo -e "\nToken Checkpoint\n" | lolcat')
             raw_input('\x1b[0;91m Enter to back ')
